7858293d90858c27f6dcc8290a7640c471d063ea
YuZhongChen2023 <a126055626@gmail.com>
2023-11-20 16:38:08 +0800

    make it better

---
+++ job.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template,request
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>資管二A 411147055 陳昱中的求職網頁</h1>"
+    homepage += "<a href=/about>我的個人簡介</a><br>"
+    homepage += "<a href=/work>資管相關工作介紹</a><br>"
+    homepage += "<a href=/ucantest>職涯測驗結果</a><br>"
+    homepage += "<a href=/autobiographyproflie>求職履歷</a><br>"
+    return homepage
+
+@app.route("/johnson")
+def about():
+    return render_template("johnson.html")
+
+@app.route("/work")
+def work():
+    return render_template("work.html")
+
+@app.route("/ucantest")
+def test():
+	return render_template("ucantest.html")
+
+@app.route("/autobiography proflie")
+def proflie():
+    return render_template("autobiography proflie.html")
+
+#if __name__ == "__main__":
+    #app.debug = True
+    #app.run()
+    
