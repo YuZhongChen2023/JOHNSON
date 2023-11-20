9f4ad8339dab3052946c1a5ec555d5d19b87f589
YuZhongChen2023 <a126055626@gmail.com>
2023-11-20 22:51:23 +0800

    make it better

--- index.py
+++ index.py
@@ -4,42 +4,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>資管二A 411147055 陳昱中的求職相關資訊</h1>"
+    homepage = "<h1>資管二A 411147055 陳昱中的求職網頁</h1>"
     homepage += "<a href=/about>我的個人簡介</a><br>"
     homepage += "<a href=/work>資管相關工作介紹</a><br>"
-    homepage += "<a href=/welcome?johnson=陳昱中>職涯測驗結果</a><br>"
-    homepage += "<a href=/proflie>求職履歷</a><br>"
+    homepage += "<a href=/ucantest>職涯測驗結果</a><br>"
+    homepage += "<a href=/autobiographyprofile>求職履歷</a><br>"
     return homepage
 
 @app.route("/about")
-def course():
-    return "<h1></h1>"
+def johnson():
+    return render_template("johnson.html")
 
-@app.route("/today")
-def today():
-    now = datetime.now()
-    return render_template("today.html", datetime = str(now))
+@app.route("/work")
+def work():
+    return render_template("work.html")
 
-@app.route("/about")
-def about():
-	return render_template("about.html")
+@app.route("/ucantest")
+def ucantest():
+	return render_template("ucantest.html")
 
-@app.route("/welcome", methods=["GET", "POST"])
-def welcome():
-    user = request.values.get("johnson")
-    return render_template("welcome.html", name=user)
+@app.route("/autobiographyprofile")
+def autobiographyprofile():
+    return render_template("autobiographyprofile.html")
 
-@app.route("/account", methods=["GET", "POST"])
-def account():
-    if request.method == "POST":
-        user = request.form["user"]
-        pwd = request.form["pwd"]
-        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
-        return result
-    else:
-        return render_template("account.html")  
-
-#if __name__ == "__main__":
-    #app.debug = True
-    #app.run()
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
     
